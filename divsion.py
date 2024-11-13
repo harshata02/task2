26760c5a3696890a5a346b6e66804d620a172420
harshata02 <padwekarharshata@gmail.com>
2024-11-13 14:13:47 +0530

    Add files via upload

---
+++ divsion.py
@@ -0,0 +1,3 @@
+def divvalue(val1=0,val2=0):
+    print('inside div value')
+    return val1/val2
---
+++ multi.py
@@ -0,0 +1,3 @@
+def productvalue(va1=0,val2=0):
+    print('inside productvalue')
+    return val1*val2
